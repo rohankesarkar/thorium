b7df809880685b1227a9c93eda1b65e52963d3e6
rohankesarkar <kesarkarrohan920@gmail.com>
2022-02-26 22:04:45 +0530

    This assignment is on mongoDB

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh87698-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://komalbansod_04:zjmLTAkg7aF42plB@komal04.fvnel.mongodb.net/rohankesarkar", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
